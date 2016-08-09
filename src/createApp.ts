9f2ebd4acbe3b3429cd6a45599a2359a6e3f699c
Mark Wubben <mark@novemberborn.net>
2016-08-09 12:50:26 +0100

    Register default store
    A default store can be provided when creating an app. This commit
    ensures it's added to the registry. Use a symbol (exported as
    `DEFAULT_STORE`) to register the store, while still requiring new stores
    to be registered using string identifiers. This prevents ID collisions.
    
    Update the various registry interfaces to accept symbols when reading
    stores. The `stateFrom` option still only takes a string, since the
    default store is already used if `stateFrom` is not provided.
    
    Fixes #27.

--- src/createApp.ts
+++ src/createApp.ts
@@ -4,6 +4,7 @@ import { EventedListener } from 'dojo-compose/mixins/createEvented';
 import { ObservableState, State } from 'dojo-compose/mixins/createStateful';
 import { Handle } from 'dojo-core/interfaces';
 import Promise from 'dojo-shim/Promise';
+import Symbol from 'dojo-shim/Symbol';
 import WeakMap from 'dojo-shim/WeakMap';
 import { Renderable } from 'dojo-widgets/mixins/createRenderable';
 
@@ -127,6 +128,9 @@ export interface ActionDefinition extends ItemDefinition<ActionFactory, ActionLi
 	 * Identifier of a store which the action should observe for its state.
 	 *
 	 * When the action is created it'll automatically observe this store.
+	 *
+	 * Note that the `DEFAULT_STORE` identifier is not supported. Default stores are automatically used if stateFrom
+	 * is not provided.
 	 */
 	stateFrom?: Identifier | StoreLike;
 }
@@ -170,6 +174,9 @@ export interface WidgetDefinition extends ItemDefinition<WidgetFactory, WidgetLi
 	 * Identifier of a store which the widget should observe for its state.
 	 *
 	 * When the widget is created, the store is passed as the `stateFrom` option.
+	 *
+	 * Note that the `DEFAULT_STORE` identifier is not supported. Default stores are automatically used if stateFrom
+	 * is not provided.
 	 */
 	stateFrom?: Identifier | StoreLike;
 
@@ -251,7 +258,7 @@ export interface CombinedRegistry {
 	 * @return A promise for when the store has been loaded. Rejected if loading fails or if no store is registered
 	 *   with the given identifier.
 	 */
-	getStore(id: Identifier): Promise<StoreLike>;
+	getStore(id: Identifier | symbol): Promise<StoreLike>;
 
 	/**
 	 * Check whether a store has been registered with the given identifier.
@@ -259,7 +266,7 @@ export interface CombinedRegistry {
 	 * @param id Identifier for the store
 	 * @return `true` if a store has been registered, `false` otherwise.
 	 */
-	hasStore(id: Identifier): boolean;
+	hasStore(id: Identifier | symbol): boolean;
 
 	/**
 	 * Look up the identifier for which the given store has been registered.
@@ -269,7 +276,7 @@ export interface CombinedRegistry {
 	 * @param store The store
 	 * @return The identifier
 	 */
-	identifyStore(store: StoreLike): Identifier;
+	identifyStore(store: StoreLike): Identifier | symbol;
 
 	/**
 	 * Get the widget with the given identifier.
@@ -442,6 +449,11 @@ const customElementFactories = new WeakMap<App, IdentityRegistry<WidgetFactory>>
 const storeFactories = new WeakMap<App, IdentityRegistry<RegisteredFactory<StoreLike>>>();
 const widgetFactories = new WeakMap<App, IdentityRegistry<RegisteredFactory<WidgetLike>>>();
 const widgetInstances = new WeakMap<App, IdentityRegistry<WidgetLike>>();
+
+/**
+ * Identifier for the default store, which is available if it was provided the app factory was created.
+ */
+export const DEFAULT_STORE = Symbol('App factory default store identifier');
 
 const createApp = compose({
 	registerAction(id: Identifier, action: ActionLike): Handle {
@@ -731,17 +743,17 @@ const createApp = compose({
 			return customElementFactories.get(this).hasId(name);
 		},
 
-		getStore(id: Identifier): Promise<StoreLike> {
+		getStore(id: Identifier | symbol): Promise<StoreLike> {
 			return new Promise((resolve) => {
 				resolve(storeFactories.get(this).get(id)());
 			});
 		},
 
-		hasStore(id: Identifier): boolean {
+		hasStore(id: Identifier | symbol): boolean {
 			return storeFactories.get(this).hasId(id);
 		},
 
-		identifyStore(store: StoreLike): string {
+		identifyStore(store: StoreLike): Identifier | symbol {
 			const app: App = this;
 			return app._instanceRegistry.identifyStore(store);
 		},
@@ -823,6 +835,11 @@ const createApp = compose({
 		storeFactories.set(instance, new IdentityRegistry<RegisteredFactory<StoreLike>>());
 		widgetFactories.set(instance, new IdentityRegistry<RegisteredFactory<WidgetLike>>());
 		widgetInstances.set(instance, new IdentityRegistry<WidgetLike>());
+
+		if (instance.defaultStore) {
+			instance._instanceRegistry.addStore(instance.defaultStore, DEFAULT_STORE);
+			storeFactories.get(instance).register(DEFAULT_STORE, () => instance.defaultStore);
+		}
 	}
 }) as AppFactory;
 
